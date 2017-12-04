6b5122f0422ed2c4493b5d6e32f14bdecd1534df
Daniel Czerwonk <daniel@dan-nrw.de>
2017-12-04 08:48:29 +0100

    fixed error handling

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/czerwonk/ovirt_api"
 )
 
-const version string = "0.1.0"
+const version string = "0.1.1"
 
 var (
 	showVersion   = flag.Bool("version", false, "Prints version info")
@@ -63,7 +63,7 @@ func errorHandler(f func(http.ResponseWriter, *http.Request) error) http.Handler
 		err := f(w, r)
 
 		if err != nil {
-			log.Fatalln(err)
+			log.Printf("ERROR: %s\n", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
