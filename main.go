40251e031b1ecc1623248b7cefdd5c75d0a5962e
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2018-05-04 16:02:16 +0200

    Update main.go
    bumped minor vers.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/czerwonk/ovirt_api/api"
 )
 
-const version string = "0.2.2"
+const version string = "0.2.3"
 
 var (
 	showVersion   = flag.Bool("version", false, "Prints version info")
